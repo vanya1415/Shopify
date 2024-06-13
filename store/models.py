a45f621401d56fe448dacd7ab860b80dcbf0a5bf
Vanya <vanya21109@iiitd.ac.in>
2024-06-13 22:50:04 +0530

    changing first_name to user_name

--- store/models.py
+++ store/models.py
@@ -23,7 +23,7 @@ class Product(models.Model):
 
 
 class Customer(models.Model):
-    first_name = models.CharField(max_length=255)
+    given_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
     email = models.EmailField(unique=True)
     phone = models.CharField(max_length=255)
