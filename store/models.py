b8e55e047a6c119fe4fad505a5bac0a898f0b7f5
Vanya <vanya21109@iiitd.ac.in>
2024-06-16 12:53:32 +0530

    Adding Django ORM files

--- store/models.py
+++ store/models.py
@@ -23,7 +23,7 @@ class Product(models.Model):
 
 
 class Customer(models.Model):
-    given_name = models.CharField(max_length=255)
+    first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
     email = models.EmailField(unique=True)
     phone = models.CharField(max_length=255)
